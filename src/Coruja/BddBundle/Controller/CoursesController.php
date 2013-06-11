794bd8a83b6b250c980d65969a8e2e5ddacbf245
Roman Sachse <sarom@web.de>
2013-06-11 11:29:29 +0200

    mache spec grün

--- src/Coruja/BddBundle/Controller/CoursesController.php
+++ src/Coruja/BddBundle/Controller/CoursesController.php
@@ -2,6 +2,12 @@
 
 namespace Coruja\BddBundle\Controller;
 
+use Symfony\Component\HttpFoundation\Response;
+
 class CoursesController
 {
+    public function listAction()
+    {
+        return new Response();
+    }
 }
