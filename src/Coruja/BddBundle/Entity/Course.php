96ff54c44bb43f30de5992f2de92aa3d1781d28b
Roman Sachse <sarom@web.de>
2013-06-11 12:00:50 +0200

    mache zu entity

--- src/Coruja/BddBundle/Entity/Course.php
+++ src/Coruja/BddBundle/Entity/Course.php
@@ -2,8 +2,18 @@
 
 namespace Coruja\BddBundle\Entity;
 
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ */
 class Course
 {
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
     protected $id;
 
     public function getId()
